0c86144dd9005e69e55c12605a316314878b2c92
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-05-30 07:31:08 -0400

    Update arith.jl

--- src/math/arith.jl
+++ src/math/arith.jl
@@ -4,6 +4,13 @@ for F in (:(+), :(-), :(*), :(/), :(\), :(^))
         $F(x::T, y::T) where {T<:MarkableFloat32} = T($F(Float32(x), Float32(y)))
         $F(x::T, y::T) where {T<:MarkableFloat16} = T($F(Float16(x), Float16(y)))
 
+        $F(x::T1, y::T2) where {T1<:MarkableFloat64, T2<:MarkableFloat64} =
+            promote_type(T1,T2)($F(Float64(x), Float64(y)))
+        $F(x::T1, y::T2) where {T1<:MarkableFloat32, T2<:MarkableFloat32} =
+            promote_type(T1,T2)($F(Float32(x), Float32(y)))
+        $F(x::T1, y::T2) where {T1<:MarkableFloat16, T2<:MarkableFloat16} =
+            promote_type(T1,T2)($F(Float16(x), Float16(y)))
+
         $F(x::T, y::R) where {T<:MarkableFloat64, R<:AbstractFloat} = T($F(Float64(x), Float64(y)))
         $F(x::T, y::R) where {T<:MarkableFloat32, R<:AbstractFloat} = T($F(Float32(x), Float32(y)))
         $F(x::T, y::R) where {T<:MarkableFloat16, R<:AbstractFloat} = T($F(Float16(x), Float16(y)))
@@ -28,6 +35,13 @@ for F in (:div, :fld, :cld, :rem, :mod)
         $F(x::T, y::T) where {T<:MarkableFloat64} = T($F(Float64(x), Float64(y)))
         $F(x::T, y::T) where {T<:MarkableFloat32} = T($F(Float32(x), Float32(y)))
         $F(x::T, y::T) where {T<:MarkableFloat16} = T($F(Float16(x), Float16(y)))
+        
+        $F(x::T1, y::T2) where {T1<:MarkableFloat64, T2<:MarkableFloat64} =
+            promote_type(T1,T2)($F(Float64(x), Float64(y)))
+        $F(x::T1, y::T2) where {T1<:MarkableFloat32, T2<:MarkableFloat32} =
+            promote_type(T1,T2)($F(Float32(x), Float32(y)))
+        $F(x::T1, y::T2) where {T1<:MarkableFloat16, T2<:MarkableFloat16} =
+            promote_type(T1,T2)($F(Float16(x), Float16(y)))
 
         $F(x::T, y::R) where {T<:MarkableFloat64, R<:AbstractFloat} = T($F(Float64(x), Float64(y)))
         $F(x::T, y::R) where {T<:MarkableFloat32, R<:AbstractFloat} = T($F(Float32(x), Float32(y)))
@@ -44,6 +58,13 @@ for F in (:divrem, :fldmod)
         $F(x::T, y::T) where {T<:MarkableFloat64} = T.($F(Float64(x), Float64(y)))
         $F(x::T, y::T) where {T<:MarkableFloat32} = T.($F(Float32(x), Float32(y)))
         $F(x::T, y::T) where {T<:MarkableFloat16} = T.($F(Float16(x), Float16(y)))
+        
+        $F(x::T1, y::T2) where {T1<:MarkableFloat64, T2<:MarkableFloat64} =
+            promote_type(T1,T2).($F(Float64(x), Float64(y)))
+        $F(x::T1, y::T2) where {T1<:MarkableFloat32, T2<:MarkableFloat32} =
+            promote_type(T1,T2).($F(Float32(x), Float32(y)))
+        $F(x::T1, y::T2) where {T1<:MarkableFloat16, T2<:MarkableFloat16} =
+            promote_type(T1,T2).($F(Float16(x), Float16(y)))
 
         $F(x::T, y::R) where {T<:MarkableFloat64, R<:AbstractFloat} = T.($F(Float64(x), Float64(y)))
         $F(x::T, y::R) where {T<:MarkableFloat32, R<:AbstractFloat} = T.($F(Float32(x), Float32(y)))
