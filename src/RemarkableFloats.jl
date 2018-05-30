8f18fdd176e0344e27d0fadd756726f70a373e83
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-05-30 03:50:07 -0400

    Update RemarkableFloats.jl

--- src/RemarkableFloats.jl
+++ src/RemarkableFloats.jl
@@ -2,16 +2,18 @@ __precompile__()
 
 module RemarkableFloats
 
+#=
 export RemarkableF64, RemarkableF32, RemarkableF16,
        @mark!, @unmark!, @addmark!, @submark!,
        ismarked, isunmarked,
        has0marks, has1mark, has2marks, has3marks,
        has0or1mark, has1or2marks, has2or3marks,
        getmark, setmark
+=#
 
 import Base: reinterpret, hash,
-    promote_rule, convert,
-    string, show,
+        promote_rule, convert,
+        string, show,
 	isfinite, isinf, isnan, issubnormal,
 	signbit, sign, abs, flipsign, copysign,
 	(==), (!=), (<), (<=), (>=), (>), isequal, isless,
