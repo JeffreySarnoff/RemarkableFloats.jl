b7de4d9629461f25c3ca1ac0ca2a21e2ec8ed735
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-05-30 07:31:37 -0400

    Update prearith.jl

--- src/math/prearith.jl
+++ src/math/prearith.jl
@@ -20,7 +20,14 @@ for F in (:flipsign, :copysign)
         $F(x::T, y::T) where {T<:MarkableFloat64} = T($F(Float64(x), Float64(y)))
         $F(x::T, y::T) where {T<:MarkableFloat32} = T($F(Float32(x), Float32(y)))
         $F(x::T, y::T) where {T<:MarkableFloat16} = T($F(Float16(x), Float16(y)))
-
+        
+        $F(x::T1, y::T2) where {T1<:MarkableFloat64, T2<:MarkableFloat64} =
+            promote_type(T1,T2)($F(Float64(x), Float64(y)))
+        $F(x::T1, y::T2) where {T1<:MarkableFloat32, T2<:MarkableFloat32} =
+            promote_type(T1,T2)($F(Float32(x), Float32(y)))
+        $F(x::T1, y::T2) where {T1<:MarkableFloat16, T2<:MarkableFloat16} =
+            promote_type(T1,T2)($F(Float16(x), Float16(y)))
+        
         $F(x::T, y::R) where {T<:MarkableFloat64, R<:AbstractFloat} = T($F(Float64(x), Float64(y)))
         $F(x::T, y::R) where {T<:MarkableFloat32, R<:AbstractFloat} = T($F(Float32(x), Float32(y)))
         $F(x::T, y::R) where {T<:MarkableFloat16, R<:AbstractFloat} = T($F(Float16(x), Float16(y)))
