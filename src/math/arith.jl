27b684966a46119593402f14b3b9260237caf862
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-05-30 07:01:26 -0400

    Create arith.jl

---
+++ src/math/arith.jl
@@ -0,0 +1,15 @@
+for F in (:(+), :(-), :(*), :(/), :(\), :(^))
+    @eval begin
+        $F(x::T, y::T) where {T<:MarkableFloat64} = $F(Float64(x), Float64(y))
+        $F(x::T, y::T) where {T<:MarkableFloat32} = $F(Float32(x), Float32(y))
+        $F(x::T, y::T) where {T<:MarkableFloat16} = $F(Float16(x), Float16(y))
+
+        $F(x::T, y::R) where {T<:MarkableFloat64, R<:AbstractFloat} = $F(Float64(x), Float64(y))
+        $F(x::T, y::R) where {T<:MarkableFloat32, R<:AbstractFloat} = $F(Float32(x), Float32(y))
+        $F(x::T, y::R) where {T<:MarkableFloat16, R<:AbstractFloat} = $F(Float16(x), Float16(y))
+
+        $F(x::R, y::T) where {T<:MarkableFloat64, R<:AbstractFloat} = $F(Float64(x), Float64(y))
+        $F(x::R, y::T) where {T<:MarkableFloat32, R<:AbstractFloat} = $F(Float32(x), Float32(y))
+        $F(x::R, y::T) where {T<:MarkableFloat16, R<:AbstractFloat} = $F(Float16(x), Float16(y))
+    end
+end
