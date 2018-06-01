c65068a0153152d47bcdd047999d5a1cbe495731
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-06-01 16:32:58 -0400

    Update markers.jl

--- src/type/markers.jl
+++ src/type/markers.jl
@@ -7,22 +7,19 @@ const Marker1 = Marker{1} ; const ʊ₁ = Marker1
 const Marker2 = Marker{2} ; const ʊ₂ = Marker2
 const Marker3 = Marker{3} ; const ʊ₃ = Marker3
 
-string(::Type{Marker0}) = "ʊ₀"
-string(::Type{Marker1}) = "ʊ₁"
-string(::Type{Marker2}) = "ʊ₂"
-string(::Type{Marker3}) = "ʊ₃"
-
-string(::Type{T})  where {T<:AbstractMarker} = string(T)
+string(::Type{Marker0}) = "ʊ₀" ; string(x::Marker0) = "ʊ₀"
+string(::Type{Marker1}) = "ʊ₁" ; string(x::Marker1) = "ʊ₁"
+string(::Type{Marker2}) = "ʊ₂" ; string(x::Marker2) = "ʊ₂"
+string(::Type{Marker3}) = "ʊ₃" ; string(x::Marker3) = "ʊ₃"
 
 show(io::IO, ::Type{Marker0}) = print(io, string(Marker0))
 show(io::IO, ::Type{Marker1}) = print(io, string(Marker1))
 show(io::IO, ::Type{Marker2}) = print(io, string(Marker2))
 show(io::IO, ::Type{Marker3}) = print(io, string(Marker3))
 
-string(x::Marker0) = "ʊ₀"
-string(x::Marker1) = "ʊ₁"
-string(x::Marker2) = "ʊ₂"
-string(x::Marker3) = "ʊ₃"
+parse(Marker, "Marker0") = Marker0a> ʊ˜ = 5
 
+parse(Marker, "Marker1") = Marker1
+parse(Marker, "Marker2") = Marker2
+parse(Marker, "Marker3") = Marker3
 
-"Food to give!"
