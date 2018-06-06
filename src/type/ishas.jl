65f7cbfe5760c1760f82b4bf1f1daeba2618e656
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-06-06 09:14:41 -0400

    Update ishas.jl

--- src/type/ishas.jl
+++ src/type/ishas.jl
@@ -10,6 +10,16 @@ marked(x::T) where {T<:Base.IEEEFloat} = false
 marked(x::T) where {N, T<:MarkableFloat{N}} = true
 marked(x::T) where {T<:MarkableFloat{0}} = false
 
+has0marks(x::T) where {T<:Base.IEEEFloat} = true
+has0or1marks(x::T) where {T<:Base.IEEEFloat} = true
+has1mark(x::T) where {T<:Base.IEEEFloat} = false
+has2marks(x::T) where {T<:Base.IEEEFloat} = false
+has3marks(x::T) where {T<:Base.IEEEFloat} = false
+has1or2marks(x::T) where {T<:Base.IEEEFloat} = false
+has1or3marks(x::T) where {T<:Base.IEEEFloat} = false
+has2or3marks(x::T) where {T<:Base.IEEEFloat} = false
+
+
 has0marks(x::T) where {T<:MarkableFloat{0}} = true
 has0marks(x::T) where {N, T<:MarkableFloat{N}} = false
 has1mark(x::T) where {T<:MarkableFloat{1}} = true
