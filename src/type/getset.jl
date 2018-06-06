5da79be3c6b9ee06021a8685d3f18b52f3733330
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-06-06 07:43:10 -0400

    Update getset.jl

--- src/type/getset.jl
+++ src/type/getset.jl
@@ -1,9 +1,9 @@
-getmark(x::T) where {T<:Base.IEEEFloat} = MARKER0
+getmark(x::T) where {T<:Base.IEEEFloat} = Marker0
 
-getmark(x::T) where {T<:MarkableFloat{0}} = MARKER0
-getmark(x::T) where {T<:MarkableFloat{1}} = MARKER1
-getmark(x::T) where {T<:MarkableFloat{2}} = MARKER2
-getmark(x::T) where {T<:MarkableFloat{3}} = MARKER3
+getmark(x::T) where {T<:MarkableFloat{0}} = Marker0
+getmark(x::T) where {T<:MarkableFloat{1}} = Marker1
+getmark(x::T) where {T<:MarkableFloat{2}} = Marker2
+getmark(x::T) where {T<:MarkableFloat{3}} = Marker3
 
 
 getmarkvalue(x::T) where {T<:Base.IEEEFloat} = 0
@@ -17,87 +17,87 @@ getmarkvalue(x::T) where {T<:MarkableFloat{3}} = 3
 @inline setmarkvalue(x::T, value::Int) where {T<:Union{Base.IEEEFloat, MarkableFloat}} =
     value in 0:3 ? setmark(x, Marker{value}) : throw(ErrorException("marker value out of range ($value)"))
 
-setmark(x::Float64, ::Type{MARKER0}) = x
-setmark(x::Float32, ::Type{MARKER0}) = x
-setmark(x::Float16, ::Type{MARKER0}) = x
+setmark(x::Float64, ::Type{Marker0}) = x
+setmark(x::Float32, ::Type{Marker0}) = x
+setmark(x::Float16, ::Type{Marker0}) = x
 
-setmark(x::Float64, ::Type{MARKER1}) = MarkableF641(x)
-setmark(x::Float32, ::Type{MARKER1}) = MarkableF321(x)
-setmark(x::Float16, ::Type{MARKER1}) = MarkableF161(x)
+setmark(x::Float64, ::Type{Marker1}) = MarkableF641(x)
+setmark(x::Float32, ::Type{Marker1}) = MarkableF321(x)
+setmark(x::Float16, ::Type{Marker1}) = MarkableF161(x)
 
-setmark(x::Float64, ::Type{MARKER2}) = MarkableF642(x)
-setmark(x::Float32, ::Type{MARKER2}) = MarkableF322(x)
-setmark(x::Float16, ::Type{MARKER2}) = MarkableF162(x)
+setmark(x::Float64, ::Type{Marker2}) = MarkableF642(x)
+setmark(x::Float32, ::Type{Marker2}) = MarkableF322(x)
+setmark(x::Float16, ::Type{Marker2}) = MarkableF162(x)
 
-setmark(x::Float64, ::Type{MARKER3}) = MarkableF643(x)
-setmark(x::Float32, ::Type{MARKER3}) = MarkableF323(x)
-setmark(x::Float16, ::Type{MARKER3}) = MarkableF163(x)
-
-
-setmark(x::MarkableF640, ::Type{MARKER0}) = x
-setmark(x::MarkableF320, ::Type{MARKER0}) = x
-setmark(x::MarkableF160, ::Type{MARKER0}) = x
-
-setmark(x::MarkableF640, ::Type{MARKER1}) = MarkableF641(x)
-setmark(x::MarkableF320, ::Type{MARKER1}) = MarkableF321(x)
-setmark(x::MarkableF160, ::Type{MARKER1}) = MarkableF161(x)
-
-setmark(x::MarkableF640, ::Type{MARKER2}) = MarkableF642(x)
-setmark(x::MarkableF320, ::Type{MARKER2}) = MarkableF322(x)
-setmark(x::MarkableF160, ::Type{MARKER2}) = MarkableF162(x)
-
-setmark(x::MarkableF640, ::Type{MARKER3}) = MarkableF643(x)
-setmark(x::MarkableF320, ::Type{MARKER3}) = MarkableF323(x)
-setmark(x::MarkableF160, ::Type{MARKER3}) = MarkableF163(x)
+setmark(x::Float64, ::Type{Marker3}) = MarkableF643(x)
+setmark(x::Float32, ::Type{Marker3}) = MarkableF323(x)
+setmark(x::Float16, ::Type{Marker3}) = MarkableF163(x)
 
 
-setmark(x::MarkableF641, ::Type{MARKER0}) = MarkableF640(x)
-setmark(x::MarkableF321, ::Type{MARKER0}) = MarkableF320(x)
-setmark(x::MarkableF161, ::Type{MARKER0}) = MarkableF160(x)
+setmark(x::MarkableF640, ::Type{Marker0}) = x
+setmark(x::MarkableF320, ::Type{Marker0}) = x
+setmark(x::MarkableF160, ::Type{Marker0}) = x
 
-setmark(x::MarkableF641, ::Type{MARKER1}) = x
-setmark(x::MarkableF321, ::Type{MARKER1}) = x
-setmark(x::MarkableF161, ::Type{MARKER1}) = x
+setmark(x::MarkableF640, ::Type{Marker1}) = MarkableF641(x)
+setmark(x::MarkableF320, ::Type{Marker1}) = MarkableF321(x)
+setmark(x::MarkableF160, ::Type{Marker1}) = MarkableF161(x)
 
-setmark(x::MarkableF641, ::Type{MARKER2}) = MarkableF642(x)
-setmark(x::MarkableF321, ::Type{MARKER2}) = MarkableF322(x)
-setmark(x::MarkableF161, ::Type{MARKER2}) = MarkableF162(x)
+setmark(x::MarkableF640, ::Type{Marker2}) = MarkableF642(x)
+setmark(x::MarkableF320, ::Type{Marker2}) = MarkableF322(x)
+setmark(x::MarkableF160, ::Type{Marker2}) = MarkableF162(x)
 
-setmark(x::MarkableF641, ::Type{MARKER3}) = MarkableF643(x)
-setmark(x::MarkableF321, ::Type{MARKER3}) = MarkableF323(x)
-setmark(x::MarkableF161, ::Type{MARKER3}) = MarkableF163(x)
-
-
-setmark(x::MarkableF642, ::Type{MARKER0}) = MarkableF640(x)
-setmark(x::MarkableF322, ::Type{MARKER0}) = MarkableF320(x)
-setmark(x::MarkableF162, ::Type{MARKER0}) = MarkableF160(x)
-
-setmark(x::MarkableF642, ::Type{MARKER1}) = MarkableF641(x)
-setmark(x::MarkableF322, ::Type{MARKER1}) = MarkableF321(x)
-setmark(x::MarkableF162, ::Type{MARKER1}) = MarkableF161(x)
-
-setmark(x::MarkableF642, ::Type{MARKER2}) = x
-setmark(x::MarkableF322, ::Type{MARKER2}) = x
-setmark(x::MarkableF162, ::Type{MARKER2}) = x
-
-setmark(x::MarkableF642, ::Type{MARKER3}) = MarkableF643(x)
-setmark(x::MarkableF322, ::Type{MARKER3}) = MarkableF323(x)
-setmark(x::MarkableF162, ::Type{MARKER3}) = MarkableF163(x)
+setmark(x::MarkableF640, ::Type{Marker3}) = MarkableF643(x)
+setmark(x::MarkableF320, ::Type{Marker3}) = MarkableF323(x)
+setmark(x::MarkableF160, ::Type{Marker3}) = MarkableF163(x)
 
 
-setmark(x::MarkableF643, ::Type{MARKER0}) = MarkableF640(x)
-setmark(x::MarkableF323, ::Type{MARKER0}) = MarkableF320(x)
-setmark(x::MarkableF163, ::Type{MARKER0}) = MarkableF160(x)
+setmark(x::MarkableF641, ::Type{Marker0}) = MarkableF640(x)
+setmark(x::MarkableF321, ::Type{Marker0}) = MarkableF320(x)
+setmark(x::MarkableF161, ::Type{Marker0}) = MarkableF160(x)
 
-setmark(x::MarkableF643, ::Type{MARKER1}) = MarkableF641(x)
-setmark(x::MarkableF323, ::Type{MARKER1}) = MarkableF321(x)
-setmark(x::MarkableF163, ::Type{MARKER1}) = MarkableF161(x)
+setmark(x::MarkableF641, ::Type{Marker1}) = x
+setmark(x::MarkableF321, ::Type{Marker1}) = x
+setmark(x::MarkableF161, ::Type{Marker1}) = x
 
-setmark(x::MarkableF643, ::Type{MARKER2}) = MarkableF642(x)
-setmark(x::MarkableF323, ::Type{MARKER2}) = MarkableF322(x)
-setmark(x::MarkableF163, ::Type{MARKER2}) = MarkableF162(x)
+setmark(x::MarkableF641, ::Type{Marker2}) = MarkableF642(x)
+setmark(x::MarkableF321, ::Type{Marker2}) = MarkableF322(x)
+setmark(x::MarkableF161, ::Type{Marker2}) = MarkableF162(x)
 
-setmark(x::MarkableF643, ::Type{MARKER3}) = x
-setmark(x::MarkableF323, ::Type{MARKER3}) = x
-setmark(x::MarkableF163, ::Type{MARKER3}) = x
+setmark(x::MarkableF641, ::Type{Marker3}) = MarkableF643(x)
+setmark(x::MarkableF321, ::Type{Marker3}) = MarkableF323(x)
+setmark(x::MarkableF161, ::Type{Marker3}) = MarkableF163(x)
+
+
+setmark(x::MarkableF642, ::Type{Marker0}) = MarkableF640(x)
+setmark(x::MarkableF322, ::Type{Marker0}) = MarkableF320(x)
+setmark(x::MarkableF162, ::Type{Marker0}) = MarkableF160(x)
+
+setmark(x::MarkableF642, ::Type{Marker1}) = MarkableF641(x)
+setmark(x::MarkableF322, ::Type{Marker1}) = MarkableF321(x)
+setmark(x::MarkableF162, ::Type{Marker1}) = MarkableF161(x)
+
+setmark(x::MarkableF642, ::Type{Marker2}) = x
+setmark(x::MarkableF322, ::Type{Marker2}) = x
+setmark(x::MarkableF162, ::Type{Marker2}) = x
+
+setmark(x::MarkableF642, ::Type{Marker3}) = MarkableF643(x)
+setmark(x::MarkableF322, ::Type{Marker3}) = MarkableF323(x)
+setmark(x::MarkableF162, ::Type{Marker3}) = MarkableF163(x)
+
+
+setmark(x::MarkableF643, ::Type{Marker0}) = MarkableF640(x)
+setmark(x::MarkableF323, ::Type{Marker0}) = MarkableF320(x)
+setmark(x::MarkableF163, ::Type{Marker0}) = MarkableF160(x)
+
+setmark(x::MarkableF643, ::Type{Marker1}) = MarkableF641(x)
+setmark(x::MarkableF323, ::Type{Marker1}) = MarkableF321(x)
+setmark(x::MarkableF163, ::Type{Marker1}) = MarkableF161(x)
+
+setmark(x::MarkableF643, ::Type{Marker2}) = MarkableF642(x)
+setmark(x::MarkableF323, ::Type{Marker2}) = MarkableF322(x)
+setmark(x::MarkableF163, ::Type{Marker2}) = MarkableF162(x)
+
+setmark(x::MarkableF643, ::Type{Marker3}) = x
+setmark(x::MarkableF323, ::Type{Marker3}) = x
+setmark(x::MarkableF163, ::Type{Marker3}) = x
 
