6e54baa89c4bd20e2c38dafde0583e01c2c789a3
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-06-06 08:47:29 -0400

    Create macros.jl

---
+++ src/type/macros.jl
@@ -0,0 +1,17 @@
+macro incrmark!(x)
+    quote
+        $(esc(x)) = incrmark($(esc(x)))
+    end
+end
+
+macro decrmark!(x)
+    quote
+        $(esc(x)) = incrmark($(esc(x)))
+    end
+end
+
+macro setmark!(x,m)
+    quote
+        $(esc(x)) = setmark($(esc(x)),$(esc(m)))
+    end
+end
