edfec12afeeb821393f8e11d6e44ae8fc8d1416c
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-04-30 10:11:45 -0400

    remove atan2

--- src/math/mathfuncs.jl
+++ src/math/mathfuncs.jl
@@ -1,6 +1,6 @@
 for F in (:acos, :acosd, :acosh, :acot, :acotd, :acoth, :acsc, :acscd, :acsch,
-          :asec, :asecd, :asech, :asin, :asind, :asinh, :atan, :atan2, :atand,
-          :atanh, :beta, :cbrt, :cos,:cosc, :cosd, :cosh, :cospi, :cot, :cotd,
+          :asec, :asecd, :asech, :asin, :asind, :asinh, :atan, :atand, :atanh,
+          :beta, :cbrt, :cos,:cosc, :cosd, :cosh, :cospi, :cot, :cotd,
           :coth, :csc, :cscd, :csch, :deg2rad, :exp, :exp10, :exp2, :expm1,
           :exponent, :frexp, :gamma, :lfact, :lgamma, :log, :log10, :log1p, :log2,
           :mod2pi, :modf, :rad2deg, :rem2pi, :sec, :secd, :sech, :significand,
