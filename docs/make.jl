8f48c0dc01a6ae1b8b677ddec52edaed4fd8d58c
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-04-30 11:53:34 -0400

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,2 +1,22 @@
-[deps]
-Documenter = "e30172f5-a6a5-5a46-863b-614d45cd2de4"
+using Documenter
+
+makedocs(;
+    modules=[RemarkableFloats],
+    authors="Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>",
+    repo="https://github.com/JeffreySarnoff/RemarkableFloats.jl",
+    sitename="RemarkableFloats.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        edit_link="main",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(;
+    repo="github.com/JeffreySarnoff/RemarkableFloats.jl",
+    push_preview=true,
+    devbranch="main",
+)
